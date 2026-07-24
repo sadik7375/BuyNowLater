3ffbd21d76ccedb8990e153bdc9428f95b782fe2
sadik7375 <68754591+sadik7375@users.noreply.github.com>
2026-07-25 01:10:57 +0600

    Fix paths in wipe_bookings.php to use absolute directory path

--- scratch/wipe_bookings.php
+++ scratch/wipe_bookings.php
@@ -1,6 +1,6 @@
 <?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
