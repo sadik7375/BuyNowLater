e8095bd72adc87f3bf7fd4f0f314548c5c80966e
sadik7375 <68754591+sadik7375@users.noreply.github.com>
2026-07-27 21:53:45 +0600

    Set public HTTPS remote URL in git-pull.php

--- public/git-pull.php
+++ public/git-pull.php
@@ -13,12 +13,15 @@ if (file_exists($baseDir . '/.git/index.lock')) {
 
 echo "<pre>\n";
 echo "Current directory: " . getcwd() . "\n\n";
-echo "--- GIT STATUS ---\n";
-echo shell_exec("git status 2>&1") . "\n\n";
+echo "Setting remote URL to public HTTPS repo...\n";
+shell_exec("git remote set-url origin https://github.com/sadik7375/BuyNowLater.git 2>&1");
+
 echo "--- GIT FETCH ---\n";
-echo shell_exec("git fetch origin 2>&1") . "\n\n";
+echo shell_exec("git fetch origin main 2>&1") . "\n\n";
+
 echo "--- GIT RESET ---\n";
 echo shell_exec("git reset --hard origin/main 2>&1") . "\n\n";
+
 echo "--- OPTIMIZE CLEAR ---\n";
 echo shell_exec("php artisan optimize:clear 2>&1");
 echo "</pre>";
