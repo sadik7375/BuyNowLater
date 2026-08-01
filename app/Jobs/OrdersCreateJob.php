7c44955f599fc7ffa07bda84e984f9be485bff8e
sadik7375 <68754591+sadik7375@users.noreply.github.com>
2026-08-01 13:55:46 +0600

    Add missing OrdersCreateJob and OrdersUpdatedJob for handling deposit partial payments from Shopify webhooks

---
+++ app/Jobs/OrdersCreateJob.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Osiset\ShopifyApp\Objects\Values\ShopDomain;
+use stdClass;
+
+class OrdersCreateJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Shop's myshopify domain
+     *
+     * @var ShopDomain|string
+     */
+    public $shopDomain;
+
+    /**
+     * The webhook data
+     *
+     * @var object
+     */
+    public $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param string   $shopDomain The shop's myshopify domain.
+     * @param stdClass $data       The webhook data (JSON decoded).
+     *
+     * @return void
+     */
+    public function __construct($shopDomain, $data)
+    {
+        $this->shopDomain = $shopDomain;
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // Proxy directly to OrdersPaidJob logic to handle partial payment / deposit on order creation
+        $job = new OrdersPaidJob($this->shopDomain, $this->data);
+        $job->handle();
+    }
+}
