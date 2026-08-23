69214f1b994c3c7dd3134fa2a53ee5a135e23bc6
sadik7375 <68754591+sadik7375@users.noreply.github.com>
2026-08-23 17:22:58 +0600

    Force fetch direct URL into FETCH_HEAD

--- public/git-pull.php
+++ public/git-pull.php
@@ -16,14 +16,12 @@ if (file_exists($baseDir . '/public/hot')) {
 
 echo "<pre>\n";
 echo "Current directory: " . getcwd() . "\n\n";
-echo "Setting remote URL to public HTTPS repo...\n";
-shell_exec("git remote set-url origin https://github.com/sadik7375/BuyNowLater.git 2>&1");
 
-echo "--- GIT FETCH --- \n";
-echo shell_exec("git fetch origin main 2>&1") . "\n\n";
+echo "--- GIT FETCH FETCH_HEAD --- \n";
+echo shell_exec("git fetch https://github.com/sadik7375/BuyNowLater.git main 2>&1") . "\n\n";
 
-echo "--- GIT RESET --- \n";
-echo shell_exec("git reset --hard origin/main 2>&1") . "\n\n";
+echo "--- GIT RESET FETCH_HEAD --- \n";
+echo shell_exec("git reset --hard FETCH_HEAD 2>&1") . "\n\n";
 
 echo "--- OPTIMIZE CLEAR ---\n";
 echo shell_exec("php artisan optimize:clear 2>&1");
