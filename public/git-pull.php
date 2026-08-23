94a9473766abe5d8e334b73be75f9827b54b318e
sadik7375 <68754591+sadik7375@users.noreply.github.com>
2026-08-23 16:58:09 +0600

    Fix git-pull.php to fetch latest origin/main

--- public/git-pull.php
+++ public/git-pull.php
@@ -19,11 +19,10 @@ echo "Current directory: " . getcwd() . "\n\n";
 echo "Setting remote URL to public HTTPS repo...\n";
 shell_exec("git remote set-url origin https://github.com/sadik7375/BuyNowLater.git 2>&1");
 
-echo "--- GIT FETCH ---\n";
-echo shell_exec("git reset --hard HEAD~5 2>&1") . "\n";
+echo "--- GIT FETCH --- \n";
 echo shell_exec("git fetch --all --prune 2>&1") . "\n\n";
 
-echo "--- GIT RESET ---\n";
+echo "--- GIT RESET --- \n";
 echo shell_exec("git reset --hard origin/main 2>&1") . "\n\n";
 
 echo "--- OPTIMIZE CLEAR ---\n";
